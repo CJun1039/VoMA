6fa74adf69d8e64d2b0e6c2148be10cfdc565327
Joel Wong <e0726053@u.nus.edu>
2024-01-31 13:50:07 +0800

    update schema

--- backend/Schemas/MySQLSchema.sql
+++ backend/Schemas/MySQLSchema.sql
@@ -1,3 +1,60 @@
+CREATE TABLE IF NOT EXISTS volunteer (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    gender VARCHAR(255) NOT NULL,
+    race VARCHAR(255) NOT NULL,
+    dob DATE NOT NULL,
+    phone VARCHAR(255) NOT NULL UNIQUE,
+    nationality VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    intrests VARCHAR(255) NOT NULL,
+    skills VARCHAR(255) NOT NULL,
+    preferedDays VARCHAR(255) NOT NULL,
+    frequency VARCHAR(255) NOT NULL,
+    area VARCHAR(255) NOT NULL,
+    language VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    NOKName VARCHAR(255) NOT NULL,
+    NOKPhone VARCHAR(255) NOT NULL UNIQUE, 
+)
+
+
+CREATE TABLE IF NOT EXISTS events (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    hours VARCHAR(255) NOT NULL,
+    cause VARCHAR(255) NOT NULL,
+    numberOfVolunteers INT NOT NULL,
+    restrictions VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    volunteerId INT,
+    FOREIGN KEY (volunteerId) REFERENCES volunteer(id)
+)
+
+
+CREATE TABLE IF NOT EXISTS workshops (
+    workshopId INT AUTO_INCREMENT PRIMARY KEY,
+    workshopName VARCHAR(255) NOT NULL,
+    workshopType VARCHAR(255) NOT NULL,
+    workshopDetails VARCHAR(255) NOT NULL,
+    eventId INT,
+    FOREIGN KEY (eventId) REFERENCES events(id)
+);
+
+
+
+// ---------------------------------------------------------
+
+
+
+
+
+
+
 CREATE TABLE IF NOT EXISTS instrument (
     instrumentId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     instrumentName VARCHAR(255) NOT NULL UNIQUE,
